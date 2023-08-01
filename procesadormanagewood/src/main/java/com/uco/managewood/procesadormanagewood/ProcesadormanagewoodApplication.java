a94f3d823de27c6484ddb146989b60598861e5c6
Miguel <miguelriostangarife@gmail.com>
2023-07-31 20:40:29 -0500

    Spring initializr procesadormanagewood

---
+++ procesadormanagewood/src/main/java/com/uco/managewood/procesadormanagewood/ProcesadormanagewoodApplication.java
@@ -0,0 +1,13 @@
+package com.uco.managewood.procesadormanagewood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesadormanagewoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesadormanagewoodApplication.class, args);
+	}
+
+}
---
+++ procesadormanagewood/src/test/java/com/uco/managewood/procesadormanagewood/ProcesadormanagewoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.managewood.procesadormanagewood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesadormanagewoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
